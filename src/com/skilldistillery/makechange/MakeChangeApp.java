03c3c00465e22c43b18937a1782d9d58f5566a90
Joseph.Dibi <jjdibiasi13@gmail.com>
2018-04-27 20:24:37 -0600

    Asking Price

--- src/com/skilldistillery/makechange/MakeChangeApp.java
+++ src/com/skilldistillery/makechange/MakeChangeApp.java
@@ -1,10 +1,26 @@
 package com.skilldistillery.makechange;
 
+import java.util.Scanner;
+
 public class MakeChangeApp {
 
+	static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		double cost = itemPrice();
+		System.out.println(cost);
+		
+		
+		
+			
+	}
 
+	private static double itemPrice() {
+		double itemCost = 0;
+		System.out.println("What was the price of your items today? ");
+		itemCost = sc.nextDouble();
+		
+		return itemCost;
 	}
 
 }
