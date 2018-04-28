be7bfa06f22f61a6d3c38a09cb28d33b736fc053
Joseph.Dibi <jjdibiasi13@gmail.com>
2018-04-28 13:22:07 -0600

    I don't think big things changed, i just wanted to commit since it had been awhile

--- src/com/skilldistillery/makechange/ChangeMaker.java
+++ src/com/skilldistillery/makechange/ChangeMaker.java
@@ -8,7 +8,7 @@ public class ChangeMaker {
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		runProgram(); 
+		runProgram();
 
 	}
 
@@ -22,31 +22,39 @@ public class ChangeMaker {
 		double pennies = .01;
 		double tenDollar = 10;
 		double fiveDollar = 5;
+		double twentyDollar = 20;
 
 		System.out.println("Your change is $" + change);
 
-		tenDollar = (int) Math.floor(change / tenDollar);
-		fiveDollar = (int) Math.floor((change - (tenDollar * 10)) / fiveDollar);
-		dollar = (int) Math.floor((change - (tenDollar * 10) - (fiveDollar * 5)) / dollar);
-		double changeLeft = change - (tenDollar * 10) - (fiveDollar * 5) - dollar;
+		twentyDollar = (int) Math.floor(change / twentyDollar);
+		tenDollar = (int) Math.floor((change - (twentyDollar * 20)) / tenDollar);
+		fiveDollar = (int) Math.floor((change - (twentyDollar * 20) - (tenDollar * 10)) / fiveDollar);
+		dollar = (int) Math.floor((change - (twentyDollar * 20) - (tenDollar * 10) - (fiveDollar * 5)) / dollar);
+		double changeLeft = change - (twentyDollar * 20) - (tenDollar * 10) - (fiveDollar * 5) - dollar;
 
-		if (tenDollar + fiveDollar + dollar > 0) {
+		if (tenDollar + fiveDollar + dollar + twentyDollar > 0) {
 			System.out.print("Your bills are: ");
+			if (twentyDollar > 1) {
+				System.out.print("" + (int) twentyDollar + " twenty dollar bills, ");
+			} else if (twentyDollar == 1) {
+				System.out.print("1 twenty dollar bill, ");
+			}
 			if (tenDollar > 1) {
-				System.out.print("" + tenDollar + " ten dollar bills, ");
+				System.out.print("" + (int) tenDollar + " ten dollar bills, ");
 			} else if (tenDollar == 1) {
 				System.out.print("1 ten dollar bill, ");
 			}
 			if (fiveDollar > 1) {
-				System.out.print("" + fiveDollar + " five dollar bills, ");
+				System.out.print("" + (int) fiveDollar + " five dollar bills, ");
 			} else if (fiveDollar == 1) {
 				System.out.print("1 five dollar bill, ");
 			}
 			if (dollar > 1) {
-				System.out.println("and " + dollar + " dollars. ");
+				System.out.println("and " + (int) dollar + " dollars");
 			} else if (dollar == 1) {
-				System.out.println("and 1 Dollar. ");
+				System.out.println("and 1 Dollar");
 			}
+			System.out.println(".");
 		}
 
 		// going to split change from dollars and add other bills
@@ -55,27 +63,29 @@ public class ChangeMaker {
 		nickel = (int) Math.floor((changeLeft - (quarter * .25) - (dime * .1)) / nickel);
 		pennies = (int) Math.floor((changeLeft - (quarter * .25) - (dime * .1) - (nickel * .05)) / pennies + .01);
 		// tiny numbers go crazy. I think this happens because I am casting to ints.
-		System.out.print("Your coins are:  ");
-
-		if (quarter > 1) {
-			System.out.print("" + quarter + " Quarters,");
-		} else if (quarter == 1) {
-			System.out.print(" one Quarter, ");
-		}
-		if (dime > 1) {
-			System.out.print("" + dime + " dimes,");
-		} else if (dime == 1) {
-			System.out.print(" one Dime, ");
-		}
-		if (nickel > 1) {
-			System.out.print("" + nickel + " Nickels,");
-		} else if (nickel == 1) {
-			System.out.print(" one Nickel, ");
-		}
-		if (pennies > 1) {
-			System.out.println(" and " + pennies + " Pennies.");
-		} else if (pennies == 1) {
-			System.out.println("and one Penny!");
+		if (changeLeft + quarter + dime + nickel + pennies > 0) {
+			System.out.print("Your coins are:  ");
+			if (quarter > 1) {
+				System.out.print("" + (int) quarter + " Quarters,");
+			} else if (quarter == 1) {
+				System.out.print(" one Quarter, ");
+			}
+			if (dime > 1) {
+				System.out.print("" + (int) dime + " dimes,");
+			} else if (dime == 1) {
+				System.out.print(" one Dime, ");
+			}
+			if (nickel > 1) {
+				System.out.print("" + (int) nickel + " Nickels,");
+			} else if (nickel == 1) {
+				System.out.print(" one Nickel, ");
+			}
+			if (pennies > 1) {
+				System.out.println(" and " + (int) pennies + " Pennies");
+			} else if (pennies == 1) {
+				System.out.println("and one Penny");
+			}
+			System.out.println(".");
 		}
 	}
 
@@ -104,9 +114,9 @@ public class ChangeMaker {
 	private static void runProgram() {
 		double itemCost, moreMoney, moneyPaid, change = 0;
 
-		System.out.print("What was the cost of the customer's items today? $");
+		System.out.print("How much did your items cost? $");
 		itemCost = sc.nextDouble();
-		System.out.print("How much did the customer pay you? $");
+		System.out.print("How much money are you going to give me? Keep in mind, I am excellent at making change. $");
 		moneyPaid = sc.nextDouble();
 		if (itemCost < moneyPaid) {
 			System.out.println("You paid: $" + moneyPaid);
@@ -114,20 +124,23 @@ public class ChangeMaker {
 			moneyTendered(change);
 		} else if (itemCost > moneyPaid) {
 			moreMoney = Math.abs(moneyPaid - itemCost);
-			System.out.println("I am sorry, but we need $" + moreMoney
-					+ " to complete this transaction. Please pay the difference.");
+			System.out.println("I am sorry, but we need at least $" + moreMoney + " to complete this transaction.");
 			double addingCorrectChange = sc.nextDouble();
 
 			while (!(itemCost == moneyPaid + addingCorrectChange || itemCost < moneyPaid + addingCorrectChange)) {
 				System.out.println("That is not the full amount, please pay at least the cost of the item.");
 				addingCorrectChange = sc.nextDouble();
 			}
-			System.out.println("Thank you for paying the full amount.");
-			change = moneyPaid + addingCorrectChange;
-			// call your tender method here
-			moneyTendered(change);
+
+			if (itemCost == moneyPaid + addingCorrectChange) {
+				System.out.println("You don't get a prize for paying exact change...");
+			} else {
+				change = moneyPaid + addingCorrectChange - itemCost;
+				// call your tender method here
+				moneyTendered(change);
+			}
 		} else {
-			System.out.println("Thank you for paying the exact amount. Have a nice day.");
+			System.out.println("You don't get a prize for paying exact change...");
 		}
 	}
 
